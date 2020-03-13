924c60070ab1e5902434174483d8a107f0d3ebb2
Erik <2883388711710@ingenieria.usac.edu.gt>
2020-03-12 20:48:38 -0600

    Envio por socket

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit} from '@angular/core';
+import { webSocket } from 'rxjs/webSocket';
 
 @Component({
   selector: 'app-navbar',
@@ -7,13 +8,23 @@ import { Component, OnInit} from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
   toggle: boolean;
+  url = 'ws://localhost:3000/';
+  url2 = 'ws://18.222.157.241:3000/';
+  subject = webSocket(this.url);
   constructor() { }
 
   ngOnInit(): void {
+    this.subject.subscribe({
+      next : (data) => alert('Alarma activada'),
+      error : console.log,
+      complete : () => {}
+    }
+    );
   }
 
   onclick(){
     console.log('working');
     this.toggle = !this.toggle;
+    this.subject.next({Data : this.toggle});
   }
 }
